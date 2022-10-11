5625ed0d8111d22508b126bce26b3bb6be3adc63
John Shaffer <john@insilica.co>
2022-10-11 17:22:40 -0500

    Use csv2parquet from nixpkgs
    Added it to nixpkgs with https://github.com/NixOS/nixpkgs/pull/194952
    
    No change in behavior, this just simplifies the code.

--- default.nix
+++ default.nix
@@ -1,26 +1,6 @@
 { sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { } }:
 with pkgs;
-let
-  jdk = pkgs.openjdk17;
-  csv2parquet = rustPlatform.buildRustPackage rec {
-    pname = "csv2parquet";
-    version = "0.6.0";
-
-    src = fetchFromGitHub {
-      owner = "domoritz";
-      repo = pname;
-      rev = "v" + version;
-      sha256 = "sha256-kb5j7d5lhElbDuoDpsijaXy3Dxjs7nRCUorkg4vKQi8=";
-    };
-
-    cargoSha256 = "sha256-rfwqLWNl05GyIBCOv9PaaYmkHBa58x0ck8Jz1qZyeos=";
-
-    meta = with lib; {
-      description = "Convert CSV files to Apache Parquet";
-      homepage = "https://github.com/domoritz/csv2parquet";
-      license = licenses.mit;
-    };
-  };
+let jdk = pkgs.openjdk17;
 in mkShell {
   buildInputs = [
     chromedriver
